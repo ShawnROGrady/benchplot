63c2d465ac02438c4eb8a36dc8437ef4ececc2f6
ShawnROGrady <shawn.r.ogrady@gmail.com>
2020-02-28 14:02:30 -0600

    [refactor] test more cases

--- plot/results_test.go
+++ plot/results_test.go
@@ -162,3 +162,184 @@ func BenchmarkSplitBenchRes(b *testing.B) {
 		})
 	}
 }
+
+var benchOutputValByNameTests = map[string]struct {
+	output                       benchparse.BenchOutputs
+	expectedIterations           int
+	expectedNsPerOp              float64
+	expectedNsPerOpErr           error
+	expectedAllocedBytesPerOp    uint64
+	expectedAllocedBytesPerOpErr error
+	expectedAllocsPerOp          uint64
+	expectedAllocsPerOpErr       error
+	expectedMBPerS               float64
+	expectedMBPerSErr            error
+}{
+	"all_set": {
+		output: newTestOutputs(
+			21801,
+			withNsPerOp(55357),
+			withAllocedBytesPerOp(4321),
+			withAllocsPerOp(21),
+			withMBPerS(0.12),
+		),
+		expectedIterations:        21801,
+		expectedNsPerOp:           55357,
+		expectedAllocedBytesPerOp: 4321,
+		expectedAllocsPerOp:       21,
+		expectedMBPerS:            0.12,
+	},
+	"benchmem_not_set_with_set_bytes": {
+		output: newTestOutputs(
+			21801,
+			withNsPerOp(55357),
+			withMBPerS(0.12),
+		),
+		expectedIterations:           21801,
+		expectedNsPerOp:              55357,
+		expectedAllocedBytesPerOpErr: benchparse.ErrNotMeasured,
+		expectedAllocsPerOpErr:       benchparse.ErrNotMeasured,
+		expectedMBPerS:               0.12,
+	},
+	"benchmem_set_but_no_allocs": {
+		output: newTestOutputs(
+			21801,
+			withNsPerOp(55357),
+			withAllocedBytesPerOp(0),
+			withAllocsPerOp(0),
+		),
+		expectedIterations:        21801,
+		expectedNsPerOp:           55357,
+		expectedAllocedBytesPerOp: 0,
+		expectedAllocsPerOp:       0,
+		expectedMBPerSErr:         benchparse.ErrNotMeasured,
+	},
+	"none_set": {
+		output:                       newTestOutputs(1),
+		expectedIterations:           1,
+		expectedNsPerOpErr:           benchparse.ErrNotMeasured,
+		expectedAllocedBytesPerOpErr: benchparse.ErrNotMeasured,
+		expectedAllocsPerOpErr:       benchparse.ErrNotMeasured,
+		expectedMBPerSErr:            benchparse.ErrNotMeasured,
+	},
+}
+
+func TestGetOutputValByName(t *testing.T) {
+	for testName, testCase := range benchOutputValByNameTests {
+		t.Run(testName, func(t *testing.T) {
+			t.Run("num_iterations", func(t *testing.T) {
+				num, err := benchOutputValByName(testCase.output, RunsName)
+				if err != nil {
+					t.Fatalf("unexpected error: %s", err)
+				}
+				if num != testCase.expectedIterations {
+					t.Errorf("unexpected iterations (expected=%d, actual=%d)", testCase.expectedIterations, num)
+				}
+			})
+			t.Run("ns_per_op", func(t *testing.T) {
+				testNsPerOp(t, testCase.output, testCase.expectedNsPerOp, testCase.expectedNsPerOpErr)
+			})
+			t.Run("allocated_bytes_per_op", func(t *testing.T) {
+				testAllocedBytesPerOp(t, testCase.output, testCase.expectedAllocedBytesPerOp, testCase.expectedAllocedBytesPerOpErr)
+			})
+			t.Run("allocs_per_op", func(t *testing.T) {
+				testAllocsPerOp(t, testCase.output, testCase.expectedAllocsPerOp, testCase.expectedAllocsPerOpErr)
+			})
+			t.Run("MB_per_s", func(t *testing.T) {
+				testMBPerS(t, testCase.output, testCase.expectedMBPerS, testCase.expectedMBPerSErr)
+			})
+		})
+	}
+}
+
+func testNsPerOp(t *testing.T, b benchparse.BenchOutputs, expectedV float64, expectedErr error) {
+	t.Helper()
+	ns, err := benchOutputValByName(b, TimeName)
+	if err != nil {
+		if expectedErr != nil {
+			if err != expectedErr {
+				t.Errorf("unexpected error received (expected=%s, actual=%s)", expectedErr, err)
+			}
+		} else {
+			t.Errorf("unexpected error: %s", err)
+		}
+		return
+	}
+
+	if expectedErr != nil {
+		t.Errorf("unexpectedly no error")
+	}
+
+	if expectedV != ns {
+		t.Errorf("unexpected NsPerOp (expected=%v, actual=%v)", expectedV, ns)
+	}
+}
+
+func testAllocedBytesPerOp(t *testing.T, b benchparse.BenchOutputs, expectedV uint64, expectedErr error) {
+	t.Helper()
+	v, err := benchOutputValByName(b, AllocBytesName)
+	if err != nil {
+		if expectedErr != nil {
+			if err != expectedErr {
+				t.Errorf("unexpected error received (expected=%s, actual=%s)", expectedErr, err)
+			}
+		} else {
+			t.Errorf("unexpected error: %s", err)
+		}
+		return
+	}
+
+	if expectedErr != nil {
+		t.Errorf("unexpectedly no error")
+	}
+
+	if expectedV != v {
+		t.Errorf("unexpected AllocedBytesPerOp (expected=%v, actual=%v)", expectedV, v)
+	}
+}
+
+func testAllocsPerOp(t *testing.T, b benchparse.BenchOutputs, expectedV uint64, expectedErr error) {
+	t.Helper()
+	v, err := benchOutputValByName(b, NumAllocsName)
+	if err != nil {
+		if expectedErr != nil {
+			if err != expectedErr {
+				t.Errorf("unexpected error received (expected=%s, actual=%s)", expectedErr, err)
+			}
+		} else {
+			t.Errorf("unexpected error: %s", err)
+		}
+		return
+	}
+
+	if expectedErr != nil {
+		t.Errorf("unexpectedly no error")
+	}
+
+	if expectedV != v {
+		t.Errorf("unexpected AllocsPerOp (expected=%v, actual=%v)", expectedV, v)
+	}
+}
+
+func testMBPerS(t *testing.T, b benchparse.BenchOutputs, expectedV float64, expectedErr error) {
+	t.Helper()
+	v, err := benchOutputValByName(b, AllocMBytesRate)
+	if err != nil {
+		if expectedErr != nil {
+			if err != expectedErr {
+				t.Errorf("unexpected error received (expected=%s, actual=%s)", expectedErr, err)
+			}
+		} else {
+			t.Errorf("unexpected error: %s", err)
+		}
+		return
+	}
+
+	if expectedErr != nil {
+		t.Errorf("unexpectedly no error")
+	}
+
+	if expectedV != v {
+		t.Errorf("unexpected MBPerS (expected=%v, actual=%v)", expectedV, v)
+	}
+}
